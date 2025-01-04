dfac795503e6e24325335aa5da7672b932f93131
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-01-04 14:59:54 +0530

    Revert "Update misc.py"
    This reverts commit 5e16fbb0b14f6f0d247c923f5143c70c0537040f.

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,12 +1,11 @@
-import os, logging, time, re
+import os, logging, time
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from info import IMDB_TEMPLATE, ADMINS, START_IMG
+from info import IMDB_TEMPLATE, ADMINS
 from speedtest import Speedtest, ConfigRetrievalError
 from utils import extract_user, get_file_id, get_poster, get_size, last_online
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from database.users_chats_db import db
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
@@ -255,48 +254,5 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
         await quer_y.message.edit(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
     await quer_y.answer()
         
-@Client.on_message(filters.command("post_mode") & filters.user(ADMINS))
-async def update_post_mode(client, message):
-    try:
-        post_mode = await db.update_post_mode_handle()
-        btn = [[
-        InlineKeyboardButton("ᴘᴏsᴛ ᴍᴏᴅᴇ ➜", callback_data="update_post_mode"),
-        InlineKeyboardButton(f"{'sɪɴɢʟᴇ' if post_mode.get('singel_post_mode', True) else 'ᴍᴜʟᴛɪ'} ᴍᴏᴅᴇ", callback_data="change_update_post_mode"),
-    ],
-    [
-        InlineKeyboardButton("ᴜᴘʟᴏᴀᴅ ᴍᴏᴅᴇ ➜", callback_data="update_post_mode"),
-        InlineKeyboardButton(f"{'ᴀʟʟ' if post_mode.get('all_files_post_mode', True) else 'ɴᴇᴡ'} ғɪʟᴇs", callback_data="all_files_post_mode"),
-    ]]
-        reply_markup = InlineKeyboardMarkup(btn)
-        await message.reply_photo(caption="<b>sᴇʟᴇᴄᴛ ᴘᴏsᴛ ᴍᴏᴅᴇ ғʀᴏᴍ ʙᴇʟᴏᴡ :</b>", photo=START_IMG, reply_markup=reply_markup)
-    except Exception as e:
-        print('Err in update_post_mode', e)
 
-@Client.on_message(filters.command("set_muc") & filters.user(ADMINS))
-async def set_muc_id(client, message):
-    try:
-        id = message.command[1]
-        if id:
-            is_suc = await db.movies_update_channel_id(int(id))
-            if is_suc:
-                await message.reply("Successfully set movies update  channel id : " + id)
-            else:
-                await message.reply("Failed to set movies update channel id : " + id)
-        else:
-            await message.reply("Invalid channel id : " + id)
-    except Exception as e:
-        print('Err in set_muc_id', e)
-        await message.reply("Failed to set movies channel id!")
-
-@Client.on_message(filters.command("del_muc") & filters.user(ADMINS))
-async def del_muc_id(client, message):
-    try:
-        is_suc = await db.del_movies_channel_id()
-        if is_suc:
-            await message.reply("Successfully deleted movies channel id")
-        else:
-            await message.reply("Failed to delete movies channel id")
-    except Exception as e:
-        print('Err in del_muc_id', e)
-        await message.reply("Failed to delete movies channel id!")
         
