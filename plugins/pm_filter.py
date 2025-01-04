0f80fa0e4318e528516e0bb6f80d29789b0c2097
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-01-04 13:29:00 +0530

    Revert "syntax error fix"
    This reverts commit b7f1520318126421bab15d074e552300cd480935.

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2750,6 +2750,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
     await query.answer(MSG_ALRT)
+
     elif query.data.startswith("update_post_mode"):
         await query.answer('Please Choose From The Given Options.', show_alert=True)
     elif query.data.startswith("change_update_post_mode"):
