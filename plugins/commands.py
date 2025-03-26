dd4f0da48812702aad72037f5ae82a3ff8d534d3
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-03-26 20:46:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,7 +24,8 @@ async def start(client, message):
         searches = message.command[1].split("-", 1)[1] 
         search = searches.replace('-',' ')
         message.text = search 
-        await auto_filter(client, message, reply_msg) 
+        reply_msg = await message.reply_text(f"<b><i>Searching For {message.text} 🔍")
+        await auto_filter(client, message.text, message, reply_msg, ai_search)
         return
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
