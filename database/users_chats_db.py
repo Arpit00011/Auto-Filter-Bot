5eb37fa43746b5f2729943b5a8aa0bbd43c20a50
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-13 16:20:47 +0530

    Revert "try"
    This reverts commit 6ca2620a56e742846b9f6e2116bf276691a90e68.

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -188,8 +188,8 @@ class Database:
         return user.get('ban_status', default)
 
     async def get_all_users(self):
-        return ((await (self.col.find({})).to_list(length=None))+(await (self.col2.find({})).to_list(length=None)))
-        
+        users_list = (await (self.col.find({})).to_list(length=None))+(await (self.col2.find({})).to_list(length=None))
+        return users_list
     
 
     async def delete_user(self, user_id):
