f9ec3936fb4763f0231ab66e82160e6445b3fee6
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-27 16:43:03 +0530

    Revert "maybe i did a mistake"
    This reverts commit 8bcacefc979542dbd520161fb3a8683f742a13cd.

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2681,7 +2681,6 @@ async def advantage_spell_chok(client, name, msg, nam_search):
         return
     movielist += [movie.get('title') for movie in movies]
     movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
-    SPELL_CHECK[mv_id] = movielist
     btn = [
         [
             InlineKeyboardButton(
